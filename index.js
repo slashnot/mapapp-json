5ccf489bdfbb1aa3255cc97f7b5fea1e9b0bea13
slashnot <slashnot84@gmail.com>
2017-01-05 17:38:49 +0530

    Started a new Data

---
+++ index.js
@@ -0,0 +1,16 @@
+var jsonServer = require('json-server');
+
+var server = jsonServer.create();
+
+server.set('port', (process.env.PORT || 5000));
+
+server.use(jsonServer.defaults());
+
+var router = jsonServer.router('data.json');
+
+
+server.use(router);
+
+server.listen(server.get('port'), function() {
+  console.log('Node app is running on port', server.get('port'));
+});
